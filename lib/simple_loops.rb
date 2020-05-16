4c41716fdb4667cb65fb902a504906a69c129810
Martin Di Vincenzo <mdivincenz@hotmail.com>
2020-05-16 02:17:10 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,9 +1,8 @@
 # Write your methods here
-
-count = 0 
-
-while 5 <count do
-  puts array[count]
+count = 0
+def loop_message_five_times (message)
+while message.length <= 5
+  puts message[count]
   count = count + 1 
 end
 
